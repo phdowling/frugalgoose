936339c43b6e9ec652231ad52b7dd3116d06efdd
Felix Sonntag <felix.sonntag@outlook.com>
2016-10-16 03:13:08 +0200

    Small updates

--- frugalgoose/views/index.jsx
+++ frugalgoose/views/index.jsx
@@ -1,25 +1,29 @@
 var React = require('react');
 var Layout = require('./layout');
+var ReactToolbox = require('react-toolbox');
+var Button = require('react-toolbox/lib/button');
+// var Button = ReactToolbox.ReactToolbox
 
 // Contrived example to show how one might use Flow type annotations
 
 
 class Index extends React.Component {
-  render() {
-    return (
-      <Layout title={this.props.title}>
-        <h1>{this.props.title}</h1>
-        <p>Welcome to {this.props.title}</p>
-        <p>
-          I can count to 10:
-        </p>
-      </Layout>
-    );
-  }
+    render() {
+        return (
+            <Layout title={this.props.title}>
+                <h1>{this.props.title}</h1>
+                <p>Welcome to {this.props.title}</p>
+                <p>
+                    I can count to 10:
+                </p>
+                <Button label="Hello World!" />
+            </Layout>
+        );
+    }
 }
 
 Index.propTypes = {
-  title: React.PropTypes.string
+    title: React.PropTypes.string
 };
 
 module.exports = Index;
